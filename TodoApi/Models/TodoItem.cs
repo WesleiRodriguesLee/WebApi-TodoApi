b7f0584e3b37009db33ada0983e72e8629931d90
Weslei_Rodrigues <rodrigueslee80@gmail.com>
2022-04-22 13:48:39 -0300

    C# API tests

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -2,5 +2,10 @@
 {
     public class TodoItem
     {
+        public long ID { get; set; }
+        public  string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+        public string? Secret { get; set; }
     }
 }
